cf0dd01b4d87455a66429e09128d267b7a735a95
fabbxible <70475105+fabbxible@users.noreply.github.com>
2021-10-06 02:17:02 +0800

    updated firmware copy script

--- buildroot/share/PlatformIO/scripts/random-bin.py
+++ buildroot/share/PlatformIO/scripts/random-bin.py
@@ -6,11 +6,11 @@ Import("env")
 
 from datetime import datetime
 env_name = str(env["PIOENV"])
-machine = {"CR10Max", "E5P", "Ender6", "CR6", "CR6MAX"}
+machine = {"CR10Max", "E5P", "E6", "E7", "CR6", "CR6Max", "CR10Smart"}
 display = "_DW74"
 if any(x in env_name for x in machine):
     #env['PROGNAME'] = "%s_DW7.4" % (env_name)
     #env['PROGNAME'] = env_name + display + datetime.now().strftime("_%Y%m%d-%H%M%S")
-    env['PROGNAME'] = env_name + display + datetime.now().strftime("_%y%m%d%H%M")
+    env['PROGNAME'] = env_name + display + datetime.now().strftime("_%y%m%d-%H%M")
 else:
-    env['PROGNAME'] = env_name + datetime.now().strftime("_%y%m%d%H%M")
+    env['PROGNAME'] = env_name + datetime.now().strftime("_%y%m%d-%H%M")
